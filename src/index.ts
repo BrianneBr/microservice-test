af7a3d55e92e70557448030bca9835d1d83116b4
BrianneBr <brianebresnahan@gmail.com>
2020-10-12 14:44:21 -0400

    Update Configuration

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.get("/", (req, res) => {
 
 // Starts the server, listening on the provided port
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Listening at http://localhost:${port}`);
 });
 
 // My code here:
