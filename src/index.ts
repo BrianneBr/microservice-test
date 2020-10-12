9d852b16ff46dcadcf085ace7a67a34aef8f212b
BrianneBr <brianebresnahan@gmail.com>
2020-10-12 14:31:10 -0400

    Express Setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("blehkjahc");
+// Express server setup
+import express from "express";
+
+// Creates an express app instance
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+// Starts the server, listening on the provided port
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// My code here:
