aa80b15660bd76b9e6af567466bd6575a6d55424
BrianneBr <brianebresnahan@gmail.com>
2020-10-15 14:55:29 -0400

    Add Initial Swagger Seetup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import AutoLoad from "fastify-autoload";
+import Swagger from "fastify-swagger";
 import * as path from "path";
 import { Database, TestDatabase } from "./database";
 
@@ -11,6 +12,50 @@ const port = 3000;
 
 // TODO
 const opts = {};
+
+// Register Swagger plugin
+app.register(Swagger, {
+	routePrefix: "/documentation",
+	swagger: {
+		info: {
+			title: "Test swagger",
+			description: "testing the fastify swagger api",
+			version: "0.1.0",
+		},
+		externalDocs: {
+			url: "https://swagger.io",
+			description: "Find more info here",
+		},
+		host: "localhost",
+		schemes: ["http"],
+		consumes: ["application/json"],
+		produces: ["application/json"],
+		tags: [
+			{ name: "user", description: "User related end-points" },
+			{ name: "code", description: "Code related end-points" },
+		],
+		definitions: {
+			User: {
+				type: "object",
+				required: ["id", "email"],
+				properties: {
+					id: { type: "string", format: "uuid" },
+					firstName: { type: "string" },
+					lastName: { type: "string" },
+					email: { type: "string", format: "email" },
+				},
+			},
+		},
+		securityDefinitions: {
+			apiKey: {
+				type: "apiKey",
+				name: "apiKey",
+				in: "header",
+			},
+		},
+	},
+	exposeRoute: true,
+});
 
 // Do not touch the following lines
 
@@ -31,6 +76,14 @@ if (false) {
 app.register(AutoLoad, {
 	dir: path.join(__dirname, "services"),
 	options: Object.assign({ prefix: "/api" }, opts),
+});
+
+// Run the swagger interface once application is ready
+app.ready((err) => {
+	if (err) {
+		throw err;
+	}
+	app.swagger();
 });
 
 // Starts the server, listening on the provided port
