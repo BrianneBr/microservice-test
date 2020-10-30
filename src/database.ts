94fada24536462c453f2458ba255ba3e488c7703
SE2Dev <brokensilencedev@gmail.com>
2020-10-29 23:17:25 -0400

    Add Database Example

--- src/database.ts
+++ src/database.ts
@@ -1,114 +1,20 @@
-import { JsonDB } from "node-json-db";
-import { Config } from "node-json-db/dist/lib/JsonDBConfig";
+import { DatabaseContext } from "./database/context";
 
-export interface User {
-	userID: number;
-	firstName: string;
-	lastName: string;
-	email: string | null;
-}
+const db = new DatabaseContext();
 
-export function TestDatabase() {
-	// The second argument is used to tell the DB to save after each push
-	// If you put false, you'll have to call the save() method.
-	// The third argument is to ask JsonDB to save the database in an human readable format. (default false)
-	// The last argument is the separator. By default it's slash (/)
-	var db = new JsonDB(new Config("database/myDataBase", true, true, "/"));
+export async function TestDatabase() {
+	await db.sequelize.sync();
 
-	// Pushing the data into the database
-	// With the wanted DataPath
-	// By default the push will override the old value
-	db.push("/test1", "super test");
+	// Add a user
+	const bob = await db.User.create({
+		firstName: "Bob",
+		lastName: "Dobalena",
+		email: null
+	});
 
-	// It also create automatically the hierarchy when pushing new data for a DataPath that doesn't exists
-	db.push("/test2/my/test", 5);
+	console.log(bob.toJSON());
 
-	// You can also push directly objects
-	db.push("/test3", { test: "test", json: { test: ["test"] } });
-
-	// If you don't want to override the data but to merge them
-	// The merge is recursive and work with Object and Array.
-	db.push(
-		"/test3",
-		{
-			new: "cool",
-			json: {
-				important: 5,
-			},
-		},
-		false
-	);
-
-	/*
-This give you this results :
-{
-   "test":"test",
-   "json":{
-	  "test":[
-		 "test"
-	  ],
-	  "important":5
-   },
-   "new":"cool"
-}
-*/
-
-	// You can't merge primitive.
-	// If you do this:
-	db.push("/test2/my/test/", 10, false);
-
-	// The data will be overriden
-
-	// Get the data from the root
-	var data = db.getData("/");
-
-	// From a particular DataPath
-	var data = db.getData("/test1");
-
-	// If you try to get some data from a DataPath that doesn't exists
-	// You'll get an Error
-	// try {
-	// 	var data = db.getData("/test1/test/dont/work");
-	// } catch (error) {
-	// 	// The error will tell you where the DataPath stopped. In this case test1
-	// 	// Since /test1/test does't exist.
-	// 	console.error(error);
-	// }
-
-	// Deleting data
-	db.delete("/test1");
-
-	// Save the data (useful if you disable the saveOnPush)
-	db.save();
-
-	// In case you have a exterior change to the databse file and want to reload it
-	// use this method
-	db.reload();
-}
-
-export class Database {
-	private readonly db: JsonDB;
-
-	constructor(filename: string) {
-		this.db = new JsonDB(
-			new Config(`database/${filename}`, true, true, "/")
-		);
-	}
-
-	/**
-	 * Returns an array of users
-	 */
-	GetUsers(): User[] {
-		try {
-			// If there are no users, return an empty array instead of undefined
-			return this.db.getData("/users") ?? [];
-		} catch {
-			return [];
-		}
-	}
-
-	/**
-	 * Adds a user to the database
-	 */
-	AddUser() { }
+	// Get a user
+	const foundUser = await db.User.findByPk(1);
+	console.log(foundUser?.toJSON());
 }
