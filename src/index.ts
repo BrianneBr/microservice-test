3d60d998bbdec38e572825b4483ad771bfc96aa5
BrianneBr <brianebresnahan@gmail.com>
2020-11-13 15:50:16 -0500

    Remove TypeORM

--- src/index.ts
+++ src/index.ts
@@ -84,28 +84,5 @@ app.listen(port, async (err, address) => {
 	await TestDatabase();
 });
 
-async function TestDatabase() {
-	createConnection({
-		type: "sqlite",
-		database: "database/testdb.sqlite",
-		entities: [
-			path.join(__dirname, "/entity/*.js")
-		],
-		synchronize: true,
-		logging: false
-	}).then(async connection => {
-		console.log("Inserting a new user into the database...");
-		const user = new User();
-		user.firstName = "Bob";
-		user.lastName = "Dobalena";
-		user.email = undefined; // Having this be undefined (either explicitly, or implcitly) will make it null in the db
-		await connection.manager.save(user);
-		console.log("Saved a new user with id: " + user.id);
-
-		console.log("Loading users from the database...");
-		const users = await connection.manager.find(User);
-		console.log("Loaded users: ", users);
-
-		console.log("Here you can setup and run express/koa/any other framework.");
-	}).catch(error => console.log(error));
+async function TestDatabase() {	
 }
